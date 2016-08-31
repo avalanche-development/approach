a7e5e28658dd666166c4723db21df92cdd552b80
jacobemerick <jpemeric@gmail.com>
2016-08-30 18:56:43 -0700

    Adds extensions

--- src/Schema/Part/Extensions.php
+++ src/Schema/Part/Extensions.php
@@ -6,5 +6,26 @@ trait Extensions
 {
 
     /** @var array */
-    protected $extensions;
+    protected $extensions = [];
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function getExtension($name)
+    {
+        if (array_key_exists($name, $this->extensions)) {
+            return $this->extensions[$name];
+        }
+        return;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function setExtension($name, $value)
+    {
+        $this->extensions[$name] = $value;
+    }
 }
--- tests/unit/src/Schema/Part/ExtensionsTest.php
+++ tests/unit/src/Schema/Part/ExtensionsTest.php
@@ -2,8 +2,74 @@
 
 namespace AvalancheDevelopment\Approach\Schema\Part;
 
+use AvalancheDevelopment\Approach\TestHelper\SetPropertyTrait;
 use PHPUnit_Framework_TestCase;
 
 class ExtensionsTest extends PHPUnit_Framework_TestCase
 {
+
+    use setPropertyTrait;
+
+    protected $extensions;
+
+    public function setUp()
+    {
+        $this->extensions = $this->getObjectForTrait(__NAMESPACE__ . '\Extensions');
+    }
+
+    public function testGetExtensionGetsExtension()
+    {
+        $extensionName = 'x-vendor-two';
+        $extensions = [
+            'x-vendor-one' => 'one',
+            'x-vendor-two' => 'two',
+        ];
+
+        $this->setProperty($this->extensions, 'extensions', $extensions);
+        $result = $this->extensions->getExtension($extensionName);
+
+        $this->assertEquals($extensions[$extensionName], $result);
+    }
+
+    public function testGetExtensionReturnsEmptyForUndefinedExtension()
+    {
+        $extensionName = 'x-vendor-three';
+        $extensions = [
+            'x-vendor-one' => 'one',
+            'x-vendor-two' => 'two',
+        ];
+
+        $this->setProperty($this->extensions, 'extensions', $extensions);
+        $result = $this->extensions->getExtension($extensionName);
+
+        $this->assertNull($result);
+    }
+
+    public function testSetExtensionSetsExtension()
+    {
+        $extensionName = 'x-vendor-name';
+        $extensionValue = 'some value';
+
+        $this->extensions->setExtension($extensionName, $extensionValue);
+
+        $this->assertAttributeEquals([
+            $extensionName => $extensionValue,
+        ], 'extensions', $this->extensions);
+    }
+
+    public function testSetsExtensionsStacksExtensions()
+    {
+        $extensionName = 'x-vendor-name';
+        $extensionValue = 'some value';
+        $prefilledExtensions = [
+            'x-vendor-prefilled' => 'other value',
+        ];
+
+        $this->setProperty($this->extensions, 'extensions', $prefilledExtensions);
+        $this->extensions->setExtension($extensionName, $extensionValue);
+
+        $this->assertAttributeEquals(array_merge($prefilledExtensions, [
+            $extensionName => $extensionValue,
+        ]), 'extensions', $this->extensions);
+    }
 }
